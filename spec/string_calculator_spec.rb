43ee936a63f28290cef7d3f70c63ea9bd54a3d25
Altruist2692 <altruist2692@gmail.com>
2024-04-23 15:24:46 +0530

    Added a class method which expects a single string argument

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,14 @@
 require_relative '../string_calculator'
 
 describe StringCalculator do
+  describe '.add' do
+    context 'method arguments' do
+      it 'expects only one string argument' do
+        expect { described_class.add('1') }.to_not raise_error
+        expect { described_class.add(1) }.to raise_error
+        expect { described_class.add(1, 2) }.to raise_error
+        expect { described_class.add('1', '2') }.to raise_error
+      end
+    end
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def self.add(numbers)
+    raise "Only single string argument is allowed." unless numbers.is_a?(String)
+  end
 end
