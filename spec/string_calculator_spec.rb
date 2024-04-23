5c47a7a74244251ff6c8219f502d541512a2b6ce
Altruist2692 <altruist2692@gmail.com>
2024-04-23 16:44:36 +0530

    Minor refactoring using static code analysis tool rubocop

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../string_calculator'
 
 describe StringCalculator do
@@ -12,44 +14,44 @@ describe StringCalculator do
     end
 
     context 'when blank string passed' do
-      it "returns 0" do
-        expect(described_class.add("")).to eq 0 
+      it 'returns 0' do
+        expect(described_class.add('')).to eq 0
       end
     end
 
     context 'when valid string with numbers' do
       it 'returns sum of passed numbers' do
-        expect(described_class.add("1")).to eq 1
-        expect(described_class.add("1,2")).to eq 3
-        expect(described_class.add("1,2,3")).to eq 6
+        expect(described_class.add('1')).to eq 1
+        expect(described_class.add('1,2')).to eq 3
+        expect(described_class.add('1,2,3')).to eq 6
       end
     end
 
-    context "when string contains valid new line character" do
+    context 'when string contains valid new line character' do
       it 'returns sum of passed numbers' do
         expect(described_class.add("1\n2,3")).to eq 6
       end
     end
 
-    context "when string contains invalid new line char" do
+    context 'when string contains invalid new line char' do
       it "return error when invalid \n passed" do
         expect(described_class.add("1,\n")).to eq 'Invalid input'
       end
     end
 
-    context "when string has different delimiter" do
-      it "considers delimiter and sum number" do
+    context 'when string has different delimiter' do
+      it 'considers delimiter and sum number' do
         expect(described_class.add("//;\n1;2")).to eq 3
       end
     end
 
-    context "when string has negative numbers" do
-      it "should raise exception with message" do
-        expect { described_class.add("-1,2,-3") }.to raise_error(RuntimeError, "Negative numbers not allowed: -1,-3")
+    context 'when string has negative numbers' do
+      it 'should raise exception with message' do
+        expect { described_class.add('-1,2,-3') }.to raise_error(RuntimeError, 'Negative numbers not allowed: -1,-3')
       end
 
       it 'does not raise an exception with correct data' do
-        expect { described_class.add("1,2,3") }.not_to raise_error
+        expect { described_class.add('1,2,3') }.not_to raise_error
       end
     end
   end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,25 +1,25 @@
+# frozen_string_literal: true
+
 require 'pry'
 class StringCalculator
-  DEFAULT_DELIMITER = ",".freeze
+  DEFAULT_DELIMITER = ','
 
   def self.add(numbers)
-    raise "Only single string argument is allowed." unless numbers.is_a?(String)
-    
-    return 0 if numbers.length == 0
+    raise 'Only single string argument is allowed.' unless numbers.is_a?(String)
+
+    return 0 if numbers.empty?
 
     delimiter = detect_delimiter(numbers)
-    numbers = numbers.gsub(/\/\/.*\n/, '').gsub(/(?<=\d)\n(?=\d)/, delimiter)
+    numbers = numbers.gsub(%r{//.*\n}, '').gsub(/(?<=\d)\n(?=\d)/, delimiter)
     elements_arr = numbers.split(Regexp.new(delimiter))
-    return 'Invalid input' unless elements_arr.all?{|element|valid?(element)}
+    return 'Invalid input' unless elements_arr.all? { |element| valid?(element) }
 
     numbers_arr = elements_arr.map(&:to_i)
-      numbers_arr.each do |num|
-        raise "Negative numbers not allowed: #{negative_numbers(numbers_arr)}" if num < 0
-      end
+    numbers_arr.each do |num|
+      raise "Negative numbers not allowed: #{negative_numbers(numbers_arr)}" if num.negative?
+    end
     numbers_arr.sum
   end
-
-  private
 
   # ensures that there is only positive or negative numbers
   # so if any extra new line chars are there then it will detect it
@@ -27,13 +27,13 @@ class StringCalculator
     !string.scan(/^[-+]?\d*\.?\d+$/).empty?
   end
 
-  # find the delimiter passed in the beginning 
+  # find the delimiter passed in the beginning
   # or else use the default delimiter
   def self.detect_delimiter(numbers)
-    numbers.start_with?("//") ? numbers[2] : DEFAULT_DELIMITER
+    numbers.start_with?('//') ? numbers[2] : DEFAULT_DELIMITER
   end
 
   def self.negative_numbers(numbers)
-    numbers.select { |num| num < 0 }.join(',')
+    numbers.select(&:negative?).join(',')
   end
 end
